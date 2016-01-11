e38130365911c4ae18f00aedf217ec00b03d3126
NakamuraDaiki <d.nakamura0717@gmail.com>
2016-01-11 14:07:19 +0900

    サービス化
    とりあえずあげる

--- app/src/main/java/jp/co/centralsoft/bstep/ratii/MainActivity.java
+++ app/src/main/java/jp/co/centralsoft/bstep/ratii/MainActivity.java
@@ -1,60 +1,72 @@
 package jp.co.centralsoft.bstep.ratii;
 
+import android.app.ActivityManager;
+import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.RemoteException;
-import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import org.altbeacon.beacon.Beacon;
-import org.altbeacon.beacon.BeaconConsumer;
-import org.altbeacon.beacon.BeaconManager;
-import org.altbeacon.beacon.BeaconParser;
-import org.altbeacon.beacon.Identifier;
-import org.altbeacon.beacon.MonitorNotifier;
-import org.altbeacon.beacon.RangeNotifier;
-import org.altbeacon.beacon.Region;
+import org.altbeacon.beacon.service.BeaconService;
 
-import java.util.Collection;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements BeaconConsumer {
+import jp.co.centralsoft.bstep.ratii.jp.co.centralsoft.bstep.ratii.service.BeaconReceiveService;
 
-    private final static String IBEACON_FORMAT = "m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24";
-    TextView destanceVal;
-    TextView messageVal;
-    Button startServiceBtn;
-    Button stopServiceBtn;
-    BeaconManager beaconManager;
-    Region region;
-    Handler mHandler = new Handler();
+public class MainActivity extends AppCompatActivity {
 
-    boolean scanFlg;
+    public final static String TAG = "Ratti";
+
+    /**
+     * 要素
+     */
+    protected TextView distanceVal;
+    protected TextView messageVal;
+    protected Button startServiceBtn;
+    protected Button stopServiceBtn;
+    /**
+     * アプリ設定
+     */
+    SharedPreferences sharedPreferences;
+    /**
+     * ビーコン受信サービス
+     */
+    Intent intentService;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        destanceVal = (TextView) findViewById(R.id.distanceVal);
+        Log.i(TAG, "アプリ起動");
+
+        // アプリの設定ファイルのロード
+        this.sharedPreferences = this.getSharedPreferences("bstep_data", Context.MODE_PRIVATE);
+
+        // 設定を元にサービスを作る
+        this.intentService = new Intent(getBaseContext(), BeaconReceiveService.class);
+        this.intentService.putExtra("uuid", this.sharedPreferences.getString("uuid", "04137D10-42C4-41D1-8E23-FCEB7FDE5856"));
+        this.intentService.putExtra("major", this.sharedPreferences.getString("major", "1"));
+        this.intentService.putExtra("minor", this.sharedPreferences.getString("minor", "1"));
+        this.intentService.putExtra("regionName", this.sharedPreferences.getString("regionName", "BSTEP"));
+
+        // 要素をひも付け
+        distanceVal = (TextView) findViewById(R.id.distanceVal);
         messageVal = (TextView) findViewById(R.id.messageVal);
         startServiceBtn = (Button) findViewById(R.id.startServiceBtn);
         stopServiceBtn = (Button) findViewById(R.id.stopServiceBtn);
-
-        initializeTextContent();
 
         // ボタンイベント(サービス開始ボタン)
         startServiceBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(!scanFlg) {
-                    startScan();
-                }
+                startService(intentService);
+                messageVal.setText(getServiceSatus() ? "サービス起動" : "サービス停止？");
             }
         });
 
@@ -62,134 +74,42 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer {
         stopServiceBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                stopScan();
-                scanFlg = false;
+                messageVal.setText("サービス停止？");
+                stopService(intentService);
+                messageVal.setText(getServiceSatus() ? "サービス起動" : "サービス停止？");
             }
         });
 
     }
 
-    protected void startScan() {
-
-        // インスタンス化
-        beaconManager = BeaconManager.getInstanceForApplication(this);
-        // パーサ設定(iBeaconのフォーマットを設定する)
-        beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout(IBEACON_FORMAT));
-
-        // uuidが"04137D10-42C4-41D1-8E23-FCEB7FDE5856"、
-        // major番号が"1"、minor番号が"1"のBeaconを監視する場合
-        Identifier uuid = Identifier.parse("04137D10-42C4-41D1-8E23-FCEB7FDE5856");
-        Identifier major = Identifier.parse("1");
-        Identifier minor = Identifier.parse("1");
-        region = new Region("BSTEP_TARGET", uuid, major, minor);
-        beaconManager.bind(this);
-    }
-
-    protected void stopScan() {
-        if(beaconManager!=null){
-            beaconManager.unbind(this); // サービスの停止
-            beaconManager = null;
-        }
-        initializeTextContent();
-    }
-
-    protected void initializeTextContent() {
-        // 初期化
-
-        destanceVal.setText("none");
-        messageVal.setText("サービス停止中");
-//        mHandler.post(new Runnable() {
-//            public void run() {
-//            }
-//        });
+    @Override
+    protected void onResume() {
+        super.onResume();
+        messageVal.setText(getServiceSatus() ? "サービス起動" : "サービス停止？");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        stopScan();
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        stopScan();
     }
 
-    @Override
-    public void onBeaconServiceConnect() {
-        try {
-            // ビーコン情報の監視を開始
-            beaconManager.startMonitoringBeaconsInRegion(region);
-        } catch (RemoteException e) {
-            e.printStackTrace();
+    boolean getServiceSatus() {
+        // サービスが実行中かチェック
+        ActivityManager am = (ActivityManager) this.getSystemService(ACTIVITY_SERVICE);
+        List<ActivityManager.RunningServiceInfo> listServiceInfo = am.getRunningServices(Integer.MAX_VALUE);
+        for (ActivityManager.RunningServiceInfo curr : listServiceInfo) {
+            // クラス名を比較
+            if (curr.service.getClassName().equals(BeaconService.class.getName())) {
+                // 実行中のサービスと一致
+//                Toast.makeText(this, "サービス実行中", Toast.LENGTH_LONG).show();
+                return true;
+            }
         }
-
-        beaconManager.setMonitorNotifier(new MonitorNotifier() {
-            @Override
-            public void didEnterRegion(Region region) {
-                // 領域への入場を検知
-
-                // レンジングの開始
-                try {
-                    beaconManager.startRangingBeaconsInRegion(region);
-                } catch (RemoteException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            @Override
-            public void didExitRegion(Region region) {
-                // 領域からの退場を検知
-                // レンジングの停止
-                try {
-                    beaconManager.stopRangingBeaconsInRegion(region);
-                } catch (RemoteException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            @Override
-            public void didDetermineStateForRegion(int i, Region region) {
-                // 領域への入退場のステータス変化を検知
-            }
-        });
-
-        beaconManager.setRangeNotifier(new RangeNotifier() {
-            @Override
-            public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
-
-                // 検出したビーコンの情報を全部Logに書き出す
-                for (final Beacon beacon : beacons) {
-                    // ログの出力
-                    Log.d("Beacon", "UUID:" + beacon.getId1() + ", major:" + beacon.getId2()
-                            + ", minor:" + beacon.getId3() + ", Distance:" + beacon.getDistance()
-                            + ",RSSI" + beacon.getRssi());
-
-                    final double distance = beacon.getDistance();
-
-                    mHandler.post(new Runnable() {
-                        public void run() {
-                            TextView distanceVal = (TextView) findViewById(R.id.distanceVal);
-                            distanceVal.setText(String.valueOf(distance));
-                            TextView messageVal = (TextView) findViewById(R.id.messageVal);
-                            if (distance < 2.0) {
-//                                messageVal.setText("ちかいよおおおお");
-                                Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
-                                long[] pattern = {0, 200, 200, 200};
-                                vibrator.vibrate(pattern, -1);
-                                Uri uri = Uri.parse("http://www.central-soft.co.jp/");
-                                Intent i = new Intent(Intent.ACTION_VIEW,uri);
-                                startActivity(i);
-                            } else {
-                                messageVal.setText("まだまだー");
-                            }
-                        }
-                    });
-                }
-            }
-        });
+        return false;
     }
-
-
 }
---
+++ app/src/main/java/jp/co/centralsoft/bstep/ratii/jp/co/centralsoft/bstep/ratii/service/BeaconReceiveService.java
@@ -0,0 +1,209 @@
+package jp.co.centralsoft.bstep.ratii.jp.co.centralsoft.bstep.ratii.service;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.Identifier;
+import org.altbeacon.beacon.MonitorNotifier;
+import org.altbeacon.beacon.RangeNotifier;
+import org.altbeacon.beacon.Region;
+
+import java.util.Collection;
+
+import jp.co.centralsoft.bstep.ratii.MainActivity;
+
+/**
+ * ビーコン受信クラス
+ * Created by Daiki Nakamura on 2016/01/11.
+ */
+public class BeaconReceiveService extends Service implements BeaconConsumer {
+
+    /**
+     * iBeaconフォーマット
+     */
+    private final static String IBEACON_FORMAT = "m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24";
+
+    /**
+     * Beaconマネージャー
+     */
+    protected BeaconManager beaconManager;
+
+    /**
+     * 通知マネージャー
+     */
+    protected NotificationManager notificationManager;
+
+    /**
+     * Beacon情報
+     */
+    protected Identifier uuid;
+    protected Identifier major;
+    protected Identifier minor;
+    protected String regionName;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // BeaconManagerのインスタンス化・IBEACONフォーマットの設定
+        beaconManager = BeaconManager.getInstanceForApplication(this);
+        beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout(IBEACON_FORMAT));
+
+        // 通知マネージャーのインスタンス化
+        notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        // Beaconの設定情報を受け取る
+        this.uuid = Identifier.parse(intent.getStringExtra("uuid"));
+        this.major = Identifier.parse(intent.getStringExtra("major"));
+        this.minor = Identifier.parse(intent.getStringExtra("minor"));
+        this.regionName = intent.getStringExtra("regionName");
+
+        // BeaconManagerをバインド(受信開始)
+        beaconManager.bind(this);
+
+        // 通知エリア
+        notificationManager.notify(MainActivity.TAG, 1, this.getNotification(this));
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        // BeaconManagerをアンバインド(受信終了)
+        if (beaconManager != null) {
+            beaconManager.unbind(this);
+            beaconManager = null;
+        }
+
+        // 通知エリアをアンバインド
+        notificationManager.cancel(MainActivity.TAG, 1);
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onBeaconServiceConnect() {
+
+        // 監視対象
+        Region region = new Region("BSTEP_TARGET", this.uuid, this.major, this.minor);
+
+        try {
+            // ビーコン情報の監視を開始
+            beaconManager.startMonitoringBeaconsInRegion(region);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+
+        // 領域監視
+        beaconManager.setMonitorNotifier(new MonitorNotifier() {
+            @Override
+            public void didEnterRegion(Region region) {
+                // 領域への入場を検知
+
+                // レンジングの開始
+                try {
+                    beaconManager.startRangingBeaconsInRegion(region);
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void didExitRegion(Region region) {
+                // 領域からの退場を検知
+                // レンジングの停止
+                try {
+                    beaconManager.stopRangingBeaconsInRegion(region);
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void didDetermineStateForRegion(int i, Region region) {
+                // 領域への入退場のステータス変化を検知
+            }
+        });
+
+        beaconManager.setRangeNotifier(new RangeNotifier() {
+            @Override
+            public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+
+                // 検出したビーコンの情報を全部Logに書き出す
+                for (final Beacon beacon : beacons) {
+                    // ログの出力
+                    Log.d("Beacon", "UUID:" + beacon.getId1() + ", major:" + beacon.getId2()
+                            + ", minor:" + beacon.getId3() + ", Distance:" + beacon.getDistance()
+                            + ",RSSI" + beacon.getRssi());
+
+//                    final double distance = beacon.getDistance();
+
+//                    mHandler.post(new Runnable() {
+//                        public void run() {
+//                            TextView distanceVal = (TextView) findViewById(R.id.distanceVal);
+//                            distanceVal.setText(String.valueOf(distance));
+//                            TextView messageVal = (TextView) findViewById(R.id.messageVal);
+//                            if (distance < 2) {
+////                                messageVal.setText("ちかいよおおおお");
+//                                Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+//                                long[] pattern = {0, 200, 200, 200};
+//                                vibrator.vibrate(pattern, -1);
+//                                Uri uri = Uri.parse("http://52.27.243.20/");
+//                                Intent i = new Intent(Intent.ACTION_VIEW, uri);
+//                                startActivity(i);
+//                            } else {
+//                                messageVal.setText("まだまだー");
+//                            }
+//                        }
+//                    });
+                }
+            }
+        });
+    }
+
+
+    private Notification getNotification(Context context) {
+        Intent notificationIntent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent =
+                PendingIntent.getActivity(context, 0, notificationIntent, 0);
+
+        // ステータスバー・通知エリアに表示する内容をセット
+        Notification.Builder builder = new Notification.Builder(context);
+        builder.setTicker("iBeacon受信サービス");
+        builder.setContentTitle("BSTEP");
+        builder.setContentText("iBeacon受信サービス起動中");
+        builder.setContentInfo("info");
+        builder.setWhen(System.currentTimeMillis());
+        builder.setContentIntent(pendingIntent);
+        builder.setSmallIcon(android.R.drawable.ic_menu_info_details);
+        Notification notification = builder.build();
+
+
+        // 実行中フラグ
+        notification.flags = Notification.FLAG_ONGOING_EVENT;
+
+        return notification;
+    }
+}
