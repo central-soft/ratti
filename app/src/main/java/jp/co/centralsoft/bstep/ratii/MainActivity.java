8fefdbd595b94e713249d513228287c85ad2745a
NakamuraDaiki <d.nakamura0717@gmail.com>
2016-01-17 14:02:13 +0900

    受信サービス修正

--- app/src/main/java/jp/co/centralsoft/bstep/ratii/MainActivity.java
+++ app/src/main/java/jp/co/centralsoft/bstep/ratii/MainActivity.java
@@ -3,8 +3,12 @@ package jp.co.centralsoft.bstep.ratii;
 import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -15,6 +19,7 @@ import org.altbeacon.beacon.service.BeaconService;
 
 import java.util.List;
 
+import jp.co.centralsoft.bstep.ratii.jp.co.centralsoft.bstep.ratii.service.BeaconPopupService;
 import jp.co.centralsoft.bstep.ratii.jp.co.centralsoft.bstep.ratii.service.BeaconReceiveService;
 
 public class MainActivity extends AppCompatActivity {
@@ -28,6 +33,16 @@ public class MainActivity extends AppCompatActivity {
     protected TextView messageVal;
     protected Button startServiceBtn;
     protected Button stopServiceBtn;
+    // サービスから値を受け取ったら動かしたい内容を書く
+    protected Handler updateHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+
+            Bundle bundle = msg.getData();
+            String message = bundle.getString("message");
+            moveWebPage();
+        }
+    };
     /**
      * アプリ設定
      */
@@ -36,7 +51,8 @@ public class MainActivity extends AppCompatActivity {
      * ビーコン受信サービス
      */
     Intent intentService;
-
+    BeaconPopupService beaconPopupService;
+    private IntentFilter intentFilter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,15 +61,8 @@ public class MainActivity extends AppCompatActivity {
 
         Log.i(TAG, "アプリ起動");
 
-        // アプリの設定ファイルのロード
-        this.sharedPreferences = this.getSharedPreferences("bstep_data", Context.MODE_PRIVATE);
-
-        // 設定を元にサービスを作る
-        this.intentService = new Intent(getBaseContext(), BeaconReceiveService.class);
-        this.intentService.putExtra("uuid", this.sharedPreferences.getString("uuid", "04137D10-42C4-41D1-8E23-FCEB7FDE5856"));
-        this.intentService.putExtra("major", this.sharedPreferences.getString("major", "1"));
-        this.intentService.putExtra("minor", this.sharedPreferences.getString("minor", "1"));
-        this.intentService.putExtra("regionName", this.sharedPreferences.getString("regionName", "BSTEP"));
+        // サービス作成
+        createBeaconReceiveService();
 
         // 要素をひも付け
         distanceVal = (TextView) findViewById(R.id.distanceVal);
@@ -65,8 +74,15 @@ public class MainActivity extends AppCompatActivity {
         startServiceBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startService(intentService);
-                messageVal.setText(getServiceSatus() ? "サービス起動" : "サービス停止？");
+                if(!getServiceSates()) {
+                    startService(intentService);
+                    beaconPopupService = new BeaconPopupService();
+                    intentFilter = new IntentFilter();
+                    intentFilter.addAction("UPDATE_ACTION");
+                    registerReceiver(beaconPopupService, intentFilter);
+                    beaconPopupService.registerHandler(updateHandler);
+                }
+                finish();
             }
         });
 
@@ -74,18 +90,18 @@ public class MainActivity extends AppCompatActivity {
         stopServiceBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                messageVal.setText("サービス停止？");
                 stopService(intentService);
-                messageVal.setText(getServiceSatus() ? "サービス起動" : "サービス停止？");
+                updateStates();
             }
         });
 
+        updateStates();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        messageVal.setText(getServiceSatus() ? "サービス起動" : "サービス停止？");
+        updateStates();
     }
 
     @Override
@@ -98,7 +114,33 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
     }
 
-    boolean getServiceSatus() {
+    protected void createBeaconReceiveService(){
+
+        // アプリの設定ファイルのロード
+        this.sharedPreferences = this.getSharedPreferences("bstep_data", Context.MODE_PRIVATE);
+
+        // 設定を元にサービスを作る
+        this.intentService = new Intent(getBaseContext(), BeaconReceiveService.class);
+        this.intentService.putExtra("uuid", this.sharedPreferences.getString("uuid", "04137D10-42C4-41D1-8E23-FCEB7FDE5856"));
+        this.intentService.putExtra("major", this.sharedPreferences.getString("major", "1"));
+        this.intentService.putExtra("minor", this.sharedPreferences.getString("minor", "1"));
+        this.intentService.putExtra("regionName", this.sharedPreferences.getString("regionName", "BSTEP"));
+    }
+
+    /**
+     * ビーコン受信サービスの状態を確認して、
+     * ステータスを更新する
+     */
+    protected void updateStates(){
+
+        messageVal.setText(getServiceSates() ? "サービス起動" : "サービス停止");
+    }
+
+    /**
+     * ビーコン受信サービスの起動状態を確認する
+     * @return true:起動中 false:未起動
+     */
+    protected boolean getServiceSates() {
         // サービスが実行中かチェック
         ActivityManager am = (ActivityManager) this.getSystemService(ACTIVITY_SERVICE);
         List<ActivityManager.RunningServiceInfo> listServiceInfo = am.getRunningServices(Integer.MAX_VALUE);
@@ -106,10 +148,19 @@ public class MainActivity extends AppCompatActivity {
             // クラス名を比較
             if (curr.service.getClassName().equals(BeaconService.class.getName())) {
                 // 実行中のサービスと一致
-//                Toast.makeText(this, "サービス実行中", Toast.LENGTH_LONG).show();
                 return true;
             }
         }
         return false;
+    }
+
+    /**
+     * セントラルソフトのホームページを起動する。
+     */
+    protected void moveWebPage() {
+
+        Uri uri = Uri.parse("http://52.27.243.20/index.php/beacon");
+        Intent i = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(i);
     }
 }
